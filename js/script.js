c7a79d811c37f21d111440f6babf5b0d3c079976
Lucy Xiao <lucyxiao@berkeley.edu>
2017-05-16 00:57:17 -0700

    updated site

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
   //Implement the "slide to left" when the user clicks on #carousel-next here
   $("#carousel-next").click( function() {
-    var curr = parseInt($("#carousel").css('margin-left').replace("px", ""));
+    var curr = parseInt($("#carousel").css('margin-left').replace("px", ""), 10);
     if (curr <= -3480) {
       return false;
 
@@ -41,7 +41,7 @@ $(document).ready(function() {
 
   //Implement the "slide to right" when the user clicks on #carousel-prev here
 $("#carousel-prev").click( function() {
-    var curr = parseInt($('#carousel').css('margin-left').replace("px", ""));
+    var curr = parseInt($('#carousel').css('margin-left').replace("px", ""), 10);
     if (curr >= 0) {
       return false;
     } else {
@@ -49,14 +49,18 @@ $("#carousel-prev").click( function() {
     }
   });
 
-function parallax() {
-  var scrolled = $(window).scrollTop();
-  $('#profile-pic').css('top', scrolled*.2+ "px");
+// function parallax() {
+//   var scrolled = $(window).scrollTop();
+  // $('#profile-pic').css('top', scrolled*0.2+ "px");
+// }
+
+// $(window).scroll(parallax);
+
+function getNavOffset() {
+  return $("nav").height();
 }
 
-$(window).scroll(parallax);
-
-  $.fn.scrollTo = function( target, options, callback ){
+  $.fn.scrollTo = function( target, options, callback){
   if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
   var settings = $.extend({
     scrollTarget  : target,
@@ -67,70 +71,64 @@ $(window).scroll(parallax);
   return this.each(function(){
     var scrollPane = $(this);
     var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
-    var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top
-    scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
+    var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top - getNavOffset()*.5;
+    scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration, 10), settings.easing, function(){
       if (typeof callback == 'function') { callback.call(this); }
     });
   });
-}
+};
 
-$("#contact.nav-item").click( function() {
+$("#contact-nav-item").click( function() {
   $('body').scrollTo('#contact-section');
-})
+});
 
-$("#skills.nav-item").click( function() {
+$("#skills-nav-item").click( function() {
   $('body').scrollTo('#skills-section');
-})
+});
 
-$("#reddit.nav-item").click( function() {
+$("#reddit-nav-item").click( function() {
   $('body').scrollTo('#reddit-section');
-})
-$("#plugd.nav-item").click( function() {
+});
+$("#plugd-nav-item").click( function() {
   $('body').scrollTo('#plugd-section');
-})
-$("#top.nav-item").click( function() {
-  $('body').scrollTo('#name-section');
-})
+});
+$("#top-nav-item").click( function() {
+  $('body').scrollTo('#top');
+});
 
-// $("#github").hover( 
-//   function() {
-//   $("#github").append("Github");
-//   }
-//   )
-
-$('.icon1').hover(
+$('#github').hover(
   function() {
-    $('.img-name1').css("display", "inline");
+    $('#github-icon').css("display", "inline");
   },
   function() {
-    $('.img-name1').css("display", "none");
+    $('#github-icon').css("display", "none");
   }
 );
 
-$('.icon2').hover(
+$('#linkedin').hover(
   function() {
-    $('.img-name2').css("display", "inline");
+    $('#linkedin-icon').css("display", "inline");
   },
   function() {
-    $('.img-name2').css("display", "none");
+    $('#linkedin-icon').css("display", "none");
   }
 );
 
-$('.icon3').hover(
+$('#email').hover(
   function() {
-    $('.img-name3').css("display", "inline");
+    $('#email-icon').css("display", "inline");
   },
   function() {
-    $('.img-name3').css("display", "none");
+    $('#email-icon').css("display", "none");
   }
 );
 
-$('.resume-text').hover(
+$('#resume').hover(
   function() {
-    $('.img-name4').css("display", "inline");
+    $('#resume-icon').css("display", "inline");
   },
   function() {
-    $('.img-name4').css("display", "none");
+    $('#resume-icon').css("display", "none");
   }
 );
 
